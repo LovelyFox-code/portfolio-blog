3f8e6033305b43eb1248f48382c248b80331df42
Alina <alinadakhno60@gmail.com>
2022-09-14 12:49:55 +0200

    Add images, svgs

--- components/atoms/Common/Colors.ts
+++
@@ -1,9 +0,0 @@
-export type Primary = "#5FB4A2" | "#203A4C" | "#33323D";
-export const Cyan: Primary = "#5FB4A2";
-export const DarkBlue: Primary = "#203A4C";
-export const GrayishDarkBlue: Primary = "#33323D";
-
-type Secondary = "#FAFAFA" | "#EAEAEB" | "#F43030"
-export const VeryLightGray: Secondary = "#FAFAFA";
-export const LightGray: Secondary = "#EAEAEB";
-export const Red: Secondary = "#F43030";
