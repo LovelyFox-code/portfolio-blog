33f89e3d8d21fd7a9764085a60404839ed306577
Alina <alinadakhno60@gmail.com>
2022-09-29 12:25:53 +0200

    add svg

--- components/Common/Footer.tsx
+++ components/Common/Footer.tsx
@@ -39,17 +39,19 @@ const Footer = () => {
         <FooterContainer>
             <FooterFlex>
                 <Row>
-                    <Logo
-                        style={height}
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="61"
-                        height="32"
-                    >
-                        <path
-                            fill="white"
-                            fill-rule="evenodd"
-                            d="M60.082 5.878L44.408 32 28.735 5.878h31.347zM15.673 0l15.674 26.122H0L15.673 0z"
-                        />
+                    <Logo style={height}>
+                        <svg
+
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="61"
+                            height="32"
+                        >
+                            <path
+                                fill="white"
+                                fill-rule="evenodd"
+                                d="M60.082 5.878L44.408 32 28.735 5.878h31.347zM15.673 0l15.674 26.122H0L15.673 0z"
+                            />
+                        </svg>
                     </Logo>
                     <FlexContainer>
                         <StyledLink href="" style={color}>
