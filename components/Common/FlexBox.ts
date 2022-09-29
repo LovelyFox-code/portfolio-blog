6dc30126522b5448fc83bcb92f712184d853a7f4
Alina <alinadakhno60@gmail.com>
2022-09-29 12:12:55 +0200

    add section style

--- components/Common/FlexBox.ts
+++ components/Common/FlexBox.ts
@@ -2,10 +2,24 @@ import styled from "styled-components";
 
 export const Row = styled.div`
   display: flex;
-  align-items: center;
+  /* align-items: center; */
   margin: 40px 0;
   justify-content: center;
 `;
 export const Column = styled.div`
   flex: 50%;
+`
+
+export const Section = styled.section`
+margin-bottom: 10%;
+width: 80%;
+margin: auto;
+`
+export const Div60 = styled.div`
+width: 60%;
+margin: auto;
+`
+export const Div80 = styled.div`
+width: 60%;
+margin: auto;
 `
