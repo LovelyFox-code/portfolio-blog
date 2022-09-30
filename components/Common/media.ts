ef14a9c8411019053fd9512750ea2b3040323866
Alina <alinadakhno60@gmail.com>
2022-09-30 17:16:44 +0200

    add media variables

---
+++ components/Common/media.ts
@@ -0,0 +1,12 @@
+export const breakpoints = {
+    sm: 20,
+    md: 30,
+    lg: 45,
+    xl: 60
+  };
+  
+  export const mediaQueries = (key: keyof typeof breakpoints) => {
+    return (style: TemplateStringsArray | String) =>
+      `@media (max-width: ${breakpoints[key]}em) { ${style} }`;
+  };
+  
