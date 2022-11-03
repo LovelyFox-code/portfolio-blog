5c6b97a80a2dcf363d3a7349785a304450317d9f
Alina <alinadakhno60@gmail.com>
2022-11-03 08:34:25 +0100

    add receive func to api

--- pages/api/contactForm.ts
+++ pages/api/contactForm.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {sendEmail} from "../../sendEmail"
+import {receiveEmail} from "../../receiveEmail"
+import {emailHTML} from "../../components/EmailHTML"
 type Data = {
   name: string
 }
@@ -8,7 +10,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const email = `
+const email = `
     <html>
     <h1 style="background-color: 'red'">Hello ${req.body.firstname} </h1>
     <p>thanks for reaching me</p>
@@ -17,7 +19,16 @@ export default function handler(
     <p>Alina</p>
     </html>
 `;
+const dataEmail =`
+  <html>
+    <h1>Hello </h1>
+    <p>${req.body.firstname}</p>
+    <p>${req.body.email}</p>
+    <p>${req.body.message}</p>
+  </html>
+`;
    console.log(req.body, req.method);
-    sendEmail(req.body.email, "Front-end", email);
+    // sendEmail(req.body.email, "Front-end", email);
+    receiveEmail("alinadakhno60@gmail.com", "Your site was visited", dataEmail)
   res.status(200).json({ name: 'John Doe' })
 }
